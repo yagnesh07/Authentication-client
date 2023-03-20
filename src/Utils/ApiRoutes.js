3698d1d51df7a11efb87f1a9b8700eb7ccae9375
gauravsharma002211 <gauravsharma002211@gmail.com>
2023-03-20 18:26:37 +0530

    change host

--- src/Utils/ApiRoutes.js
+++ src/Utils/ApiRoutes.js
@@ -1,3 +1,3 @@
-export const HOST = process.env.HOST || "https://localhost:8000";
+export const HOST = process.env.HOST_NAME || "https://localhost:8001";
 export const REGISTER_ROUTE = `${HOST}/api/auth/register`;
 export const LOGIN_ROUTE = `${HOST}/api/auth/login`;
