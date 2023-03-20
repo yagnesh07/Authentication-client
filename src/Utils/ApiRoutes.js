972fed744788c0d818d6b4e4fa4e4eab0c50c6fd
gauravsharma002211 <gauravsharma002211@gmail.com>
2023-03-20 14:09:14 +0530

    remove host

--- src/Utils/ApiRoutes.js
+++ src/Utils/ApiRoutes.js
@@ -1,3 +1,3 @@
-export const HOST = process.env.HOST || "http://localhost:8000";
+export const HOST = process.env.HOST;
 export const REGISTER_ROUTE = `${HOST}/api/auth/register`;
 export const LOGIN_ROUTE = `${HOST}/api/auth/login`;
